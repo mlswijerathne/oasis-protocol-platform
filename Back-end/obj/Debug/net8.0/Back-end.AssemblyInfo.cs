23deb1a59fea80c33b5d3893dfa1bd5d0a9eb11e
Jayani adikari <jayaniadikari19@gmail.com>
2025-07-16 15:29:00 +0530

    create register page

--- Back-end/obj/Debug/net8.0/Back-end.AssemblyInfo.cs
+++ Back-end/obj/Debug/net8.0/Back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7170b0822ba99aa36f1b52e5fc7f42d573ffa025")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee1cb39f2cb8fa828596342e25c421fefd315c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
